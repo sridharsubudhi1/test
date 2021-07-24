2612c96e1462b6ca42aef7a9956791bdb6e8a1a8
akash1 <sridharsubudhi2@gmail.com>
2021-07-24 11:30:02 -0700

    created Test.java by theTL.

---
+++ test/src/com/gaga/test/Trial.java
@@ -0,0 +1,10 @@
+package com.gaga.test;
+
+import com.gaga.beans.Utility;
+
+public class Trial {
+    public static void main(string[] args) {
+    	Utility u = new utility();
+    	System.out.println(u.getSystemDate());
+    }
+}
